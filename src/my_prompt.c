c5a599a8f54b46b1681f38cc2f48fcb9d653be84
Lindon ALIU <lindon.aliu@epitech.eu>
2022-05-25 16:33:14 +0200

    fix: name of variable in function of prompt

--- src/my_prompt.c
+++ src/my_prompt.c
@@ -15,21 +15,21 @@
 
 void my_prompt(char **env)
 {
-    char str[4096];
-    char *str2 = NULL;
+    char filepath[4096];
+    char *tmp = NULL;
     char **prompt = NULL;
 
     if (!isatty(INPUT))
         return;
-    getcwd(str, 4096);
-    str2 = malloc(sizeof(char) * (len_array_1d(str) + 1));
-    if (str == NULL || str2 == NULL)
+    getcwd(filepath, 4096);
+    tmp = malloc(sizeof(char) * (len_array_1d(filepath) + 1));
+    if (filepath == NULL || tmp == NULL)
         return;
-    str2 = my_strcpy(str2, str);
-    prompt = my_stwa_separator(str2, "/");
+    tmp = my_strcpy(tmp, filepath);
+    prompt = my_stwa_separator(tmp, "/");
     my_printf("%s[%s%s%s@%s%s %s%s%s]%s$%s>%s ", yellow, green,
         my_get_line_env(env, "USER="), yellow, purple,
         my_get_line_env(env, "HOSTNAME="), red,
         prompt[my_len_array(prompt) - 1], yellow, cyan, yellow, white);
-    my_freef("%s%t", str2, prompt);
+    my_freef("%s%t", tmp, prompt);
 }
