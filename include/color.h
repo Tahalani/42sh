4457da182a504ead78dccebdab29abc4b806cb4d
Lindon ALIU <lindon.aliu@epitech.eu>
2022-05-25 17:41:22 +0200

    feat: add define for text color

--- include/color.h
+++ include/color.h
@@ -8,11 +8,18 @@
 #ifndef COLOR_H_
     #define COLOR_H_
 
-static char const *red = "\033[0;31m";
-static char const *green = "\033[0;32m";
-static char const *yellow = "\033[0;33m";
-static char const *purple = "\033[10;95m";
-static char const *cyan = "\033[0;36m";
-static char const *white = "\033[0;37m";
+    #define RED "\033[0;31m"
+    #define GREEN "\033[0;32m"
+    #define YELLOW "\033[0;33m"
+    #define PURPLE "\033[10;95m"
+    #define CYAN "\033[0;36m"
+    #define WHITE "\033[0;37m"
+    
+    #define RED_TXT(text) "\033[0;31m" text "\033[0;37m"
+    #define GREEN_TXT(text) "\033[0;32m" text "\033[0;37m"
+    #define YELLOW_TXT(text) "\033[0;33m" text "\033[0;37m"
+    #define PUPRLE_TXT(text) "\033[0;95m" text "\033[0;37m"
+    #define CYAN_TXT(text) "\033[0;36m" text "\033[0;37m"
+    #define WHITE_TXT(text) "\033[0;37m" text "\033[0;37m"
 
-#endif/* !COLOR_H_ */
+#endif /* !COLOR_H_ */
