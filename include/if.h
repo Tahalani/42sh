9d02c89721d69bbad96e7117f852173cb1ab0bb7
Taha ALANI <taha.alani@epitech.eu>
2022-05-29 00:25:36 +0200

    edit: if.h add prototype in 1 line its more clean

--- include/if.h
+++ include/if.h
@@ -12,22 +12,25 @@
 
 int launch_if_getline(shell_t *save, char **command_after_comma);
 
-int if_flag_e_a(char const *commands, char **command_array, shell_t *save);
-
-int if_flag_b(char const *commands, char **command_array, shell_t *save);
-
-int if_flag_c(char const *commands, char **command_array, shell_t *save);
-
-int if_flag_d(char const *commands, char **command_array, shell_t *save);
-
-int if_flag_f(char const *commands, char **command_array, shell_t *save);
-
-int if_flag_r(char const *commands, char **command_array, shell_t *save);
-
-int if_flag_w(char const *commands, char **command_array, shell_t *save);
-
-int if_flag_x(char const *commands, char **command_array, shell_t *save);
-
 int manage_if(char const *commands, shell_t *save);
+
+typedef int if_flags_t(const char *commands,
+    char **commands_array, shell_t *save);
+
+if_flags_t if_flag_e_a;
+
+if_flags_t if_flag_b;
+
+if_flags_t if_flag_c;
+
+if_flags_t if_flag_d;
+
+if_flags_t if_flag_f;
+
+if_flags_t if_flag_r;
+
+if_flags_t if_flag_w;
+
+if_flags_t if_flag_x;
 
 #endif/* !IF_H_ */
